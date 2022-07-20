36dd6d70e180491dcd818de74c984abfb14054d6
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-20 17:46:55 -0600

    refactored tests and rails helper to handle vcr/webmock

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'MerchantService', :vcr do
-  it 'retrieves search data and parses response', :vcr do
+  it 'retrieves all merchant data and parses response', :vcr do
 
     parsed_json = MerchantService.all_merchants
-    
+
     expect(parsed_json).to be_a Array
     expect(parsed_json[0]).to be_a Hash
     expect(parsed_json[0][:type]).to eq('merchant')
+  end
+
+  it 'retrieves one merchants details data and parses response', :vcr do
+
+    parsed_json = MerchantService.merchant_details(1)
+    
+    expect(parsed_json).to be_a Hash
+    expect(parsed_json[:type]).to eq('merchant')
   end
 end
